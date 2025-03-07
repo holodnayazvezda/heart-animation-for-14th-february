73d018d44998df814818e078d9e9d05fdb9f0ca5
dany <holodnayazvezda@yandex.ru>
2025-03-07 18:33:32 +0300

    8 march!

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask("heart server")
 
 
-@app.route('/heart')
-def hello():
-    return render_template("index.html")
-    # return "hello"
+@app.route('/love-with-compliments')
+def love_with_compliments():
+    return render_template("index-with-text.html")
 
-app.run(host='0.0.0.0', port=8080)
+@app.route('/love-with-emojis')
+def love_with_emojis():
+    return render_template("index-without-text.html")
+
+
+if __name__ == "__main__":
+        app.run(host='0.0.0.0', port=8080)
